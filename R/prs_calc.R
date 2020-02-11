02d10f1c1684bcea20683ece762bd6ecf33e923d
Tim <timothy.a.myers@gmail.com>
2020-02-11 14:18:43 -0500

    updates to prs_calc

--- R/prs_calc.R
+++ R/prs_calc.R
@@ -6,12 +6,12 @@
 library(BEDMatrix)
 
 ####### helper code to delete later
-base <- "./data/Height.QC.Transformed_trunc"
+base_file <- "./data/Height.QC.Transformed_trunc"
 base_header = TRUE
-base_data <- read.table(file = base, header = base_header)
+base_data <- read.table(file = base_file, header = base_header)
 
-target_root <- ".data/EUR.QC"
-target_bed <- paste(target_root, ".bed", sep = "")
+target_bed <- "./data/EUR.QC.bed"
+target_data <- BEDMatrix::BEDMatrix(target_bed)
 
 
 
@@ -19,18 +19,27 @@ target_bed <- paste(target_root, ".bed", sep = "")
 
 
 # Primary PRS function
-prs_calc <- function(base = NULL, base_header = TRUE, target_root = NULL) {
+prs_calc <- function(base_file = NULL, base_header = TRUE, target_bed = NULL) {
   
   # read the 'base' data file
-  base_data <- read.table(file = base, header = base_header)
-  
+    base_data <- read.table(file = base_file, header = base_header)
   # read the 'target' data file
-  ## paste plink filenames with .bed, .bim, .fam files extensions,
-  ## using 'target_root' argument
-  target_bed <- paste(target_root, ".bed", sep = "")
-  target_bim <- paste(target_root, ".bim", sep = "")
-  target_fam <- paste(target_root, ".fam", sep = "")
+    target_data <- BEDMatrix::BEDMatrix(target_bed)
   
+  # Rename the column names in 'table_data' to match the rsID format
+  # in 'base_data'
+    colnames(target_data) = gsub(pattern = "_.", 
+                                 replacement = "", 
+                                 x = colnames(target_data)
+                                )
+   
+  # Transpose 'target_data; make rows rsID's and columns sampleID's; same
+  # as 'base_data' ('target_data' must be a matrix)
+    target_data <- t(as.matrix(target_data))
   
-  
+  # Subset 'base_data' with only rsID's and OR(weight)
+    base_data_subset <- data.frame(SNP = base_data$SNP, OR = base_data$OR)
+
+    
+    
 }
